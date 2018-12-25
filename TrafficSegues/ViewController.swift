c36432853d6c48582caca183c7fd133132b57d7c
Mark Meretzky <mark.meretzky@gmail.com>
2018-12-25 09:54:15 -0500

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Mark Meretzky on 12/25/18.
+//  Copyright © 2018 New York University School of Professional Studies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
